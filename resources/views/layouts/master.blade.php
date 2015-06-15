79b04039d4365230939a6ae7d1aa1665dd8243f6
ZaL <z4ls@live.com>
2015-06-15 15:32:04 +0000

    tweak jplayer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,36 +19,18 @@
     <meta name="viewport" content="width=device-width">
 
     @section('style')
-        <style>
-            @import url(http://fonts.googleapis.com/earlyaccess/droidarabickufi.css);
-            html,body {
-                font-family: 'Droid Arabic Kufi' !important;
-            }
-            h1,h2,h3,h4,span,p,div,table {
-                font-family: 'Droid Arabic Kufi' !important;
-            }
-        </style>
-        <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="/css/bootstrap-flat.css" rel="stylesheet">
-        <link href="/css/bootstrap.min.css" rel="stylesheet">
-        @if(App::getLocale() == 'ar')
-            <link href="/bower_components/bootstrap-rtl/dist/css/bootstrap-rtl.css" rel="stylesheet">
-        @endif
-        <link rel="stylesheet" href="/bower_components/fontawesome/css/font-awesome.min.css" type="text/css"/>
+        <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/bower_components/bootstrap-rtl/dist/css/bootstrap-rtl.css">
+        <link rel="stylesheet" href="/bower_components/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/css/jplayer.css">
         <link rel="stylesheet" href="/css/style.css">
-
     @show
 
     @section('script')
-        <script src="/js/config.js"></script>
-        <script src="/bower_components/modernizr/modernizr.js"></script>
-
-        <!-- jQuery -->
         <script src="/bower_components/jquery/dist/jquery.min.js"></script>
-
-        <!-- Bootstrap Core JavaScript -->
         <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
+        <script src="/bower_components/jplayer/dist/jplayer/jquery.jplayer.min.js"></script>
+        <script src="/bower_components/jquery-ui/jquery-ui.min.js"></script>
         <script src="/js/script.js"></script>
     @show
 
