ff8a13fdb1d325eb645f0514e01d0b89c0b830ad
ZaL <z4ls@live.com>
2015-06-16 16:18:40 +0300

    change timezone

--- config/app.php
+++ config/app.php
@@ -37,7 +37,7 @@ return [
     |
     */
 
-    'timezone'        => 'UTC',
+    'timezone'        => 'Asia/Kuwait',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
