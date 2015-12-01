67139370e8b1bfa0791bc447b2dd37638bce5b0f
ZaL <z4ls@live.com>
2015-12-01 15:35:21 +0300

    added admin

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-static">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="{{ url('home') }}"><i class="fa fa-home"></i><b> {{ trans('word.home') }}</b></a>
+            <a class="navbar-brand" href="{{ url('home') }}"><i class="fa fa-home"></i><b> Fiqhussunna </b></a>
             <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <i class="fa fa-chevron-down"></i>
             </a>
@@ -11,8 +11,8 @@
                 @include('partials.article-category-menu')
                 @include('partials.track-category-menu')
 
-                <li><a class="{{ (Request::segment('1') == 'about' ? 'active' :  false ) }}"
-                       href="{{ action('PageController@getAbout') }}">{{ trans('word.about_us') }}</a></li>
+                {{--<li><a class="{{ (Request::segment('1') == 'about' ? 'active' :  false ) }}"--}}
+                       {{--href="{{ action('PageController@getAbout') }}">{{ trans('word.about_us') }}</a></li>--}}
                 {{--<li><a class="{{ (Request::segment('1') == 'contact' ? 'active' :  false ) }}"--}}
                        {{--href="{{ action('PageController@getContact') }}">{{ trans('word.contact_us') }}</a></li>--}}
             </ul>
