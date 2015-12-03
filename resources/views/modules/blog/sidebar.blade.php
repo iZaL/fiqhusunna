32b79d9827bb534478264bf21473519c2001d0b0
ZaL <z4ls@live.com>
2015-12-04 02:24:03 +0300

    add padding

--- resources/views/modules/blog/sidebar.blade.php
+++ resources/views/modules/blog/sidebar.blade.php
@@ -6,17 +6,21 @@
             style="display: block; position: static; margin-bottom: 5px; *width: 180px;">
 
             @foreach($categories as $category)
-                <li class="dropdown-submenu"><a href="{{ action('CategoryController@getArticle',$category->id) }}"><i
+                <li class="
+                    @if(count($category->childCategories))
+                        dropdown-submenu
+                    @endif
+                        "><a href="{{ action('CategoryController@getArticle',$category->id) }}"><i
                                 class="fa fa-folder"></i> {{ ucfirst($category->name) }}</a>
-                @if($category->childCategories)
-                    <ul class="dropdown-menu">
-                        @foreach($category->childCategories as $child)
-                            <li>
-                                <a href="{{ action('CategoryController@getArticle',$child->id) }}">{{ $child->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
+                    @if($category->childCategories)
+                        <ul class="dropdown-menu">
+                            @foreach($category->childCategories as $child)
+                                <li>
+                                    <a href="{{ action('CategoryController@getArticle',$child->id) }}">{{ $child->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </li>
             @endforeach
         </ul>
