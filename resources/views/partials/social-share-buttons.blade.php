8ccbd9b9fbc06c044f2d888eb553105acbc6269d
ZaL <z4ls@live.com>
2015-12-04 02:18:38 +0300

    add padding

--- resources/views/partials/social-share-buttons.blade.php
+++ resources/views/partials/social-share-buttons.blade.php
@@ -1,5 +1,5 @@
-<div class="mTop10 mBottom10">
-    <div class="addthis_native_toolbox"></div>
+<div class="mTop10 mBottom10" style="padding-top: 10px">
+    <div class="addthis_native_toolbox mTop10 mBottom10"></div>
     {{--<span class='st_facebook_hcount' displayText='Facebook'></span>--}}
     {{--<span class='st_twitter_hcount' displayText='Tweet'></span>--}}
     {{--<span class='st_email_hcount' displayText='Email'></span>--}}
